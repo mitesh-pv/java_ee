806208d4346a5a1b78c626f61b13cd8b2f16aa0c
mitesh-pv <mitesh.vishwakarma@gmail.com>
2019-06-30 00:25:52 +0530

    Bean configured and container created

--- spring_getting_started/src/com/spring/spring_getting_started_2/Main.java
+++ spring_getting_started/src/com/spring/spring_getting_started_2/Main.java
@@ -1,12 +1,19 @@
 package com.spring.spring_getting_started_2;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Vehicle vehicle =new Bike();
+		Vehicle vehicleOne =new Bike();
 		
-		System.out.println(vehicle.getTotalDistanceTravelled());
+		System.out.println(vehicleOne.getTotalDistanceTravelled());
+		
+	 	ClassPathXmlApplicationContext vehicleContext=new ClassPathXmlApplicationContext("applicationContext.xml");
+	 	Vehicle vehicleTwo=vehicleContext.getBean("vehicle", Vehicle.class);
+	 	
+	 	System.out.println(vehicleTwo.getTotalDistanceTravelled());
+		
 	}
 }
