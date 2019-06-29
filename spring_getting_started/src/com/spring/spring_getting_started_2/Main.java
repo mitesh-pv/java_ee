ade34539998f50272435e789b2611fd19535a3c0
mitesh-pv <mitesh.vishwakarma@gmail.com>
2019-06-30 00:36:49 +0530

    inversion of control

--- spring_getting_started/src/com/spring/spring_getting_started_2/Main.java
+++ spring_getting_started/src/com/spring/spring_getting_started_2/Main.java
@@ -6,14 +6,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		// simple method of object creation		
 		Vehicle vehicleOne =new Bike();
-		
 		System.out.println(vehicleOne.getTotalDistanceTravelled());
 		
+		
+		// bean factory method of object creation
 	 	ClassPathXmlApplicationContext vehicleContext=new ClassPathXmlApplicationContext("applicationContext.xml");
 	 	Vehicle vehicleTwo=vehicleContext.getBean("vehicle", Vehicle.class);
 	 	
 	 	System.out.println(vehicleTwo.getTotalDistanceTravelled());
 		
+	 	vehicleContext.close();
 	}
 }
